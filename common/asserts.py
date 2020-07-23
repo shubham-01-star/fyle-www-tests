c0c9bbcd0d890b289919af2cd3a3d491e92f4779
snarayanank2 <siva@fyle.in>
2020-07-23 22:07:43 +0530

    Common typography asserts

---
+++ common/asserts.py
@@ -0,0 +1,55 @@
+import time
+from simplebrowser import SimpleBrowser
+import logging
+import pytest
+
+logger = logging.getLogger(__name__)
+
+def assert_hero_section(browser, section):
+    h1s = section.find_elements_by_xpath('.//h1')
+    assert len(h1s) == 1, 'Hero section should have 1 h1'
+    h1 = h1s[0]
+    font_size = h1.value_of_css_property('font-size')
+    font_weight = h1.value_of_css_property('font-weight')
+    if browser.is_desktop():
+        assert font_size == '50px', 'Hero section h1 font size is wrong'
+    else:
+        assert font_size == '30px', 'Hero section h1 font size is wrong'
+    h2s = section.find_elements_by_xpath('.//h2')
+    assert len(h2s) == 0, 'Hero section should have no h2s'
+
+
+def assert_other_section(browser, section):
+    cl = section.get_attribute('class')
+    h2s = section.find_elements_by_xpath('.//h2')
+    assert len(h2s) == 1, f'Section with class {cl} has {len(h2s)} h2s'
+    h2 = h2s[0]
+    text = h2.text
+    font_size = h2.value_of_css_property('font-size')
+    font_weight = h2.value_of_css_property('font-weight')
+    is_logo_section = 'Loved by leading finance' in text
+    if browser.is_desktop():
+        if is_logo_section:
+            assert font_size == '24px', 'Font size of logo section is wrong'
+        else:
+            assert font_size == '30px', f'Font size of h2 is wrong for {text}'
+    else:
+        if is_logo_section:
+            assert font_size == '16px', 'Font size of logo section is wrong'
+        else:
+            assert font_size == '20px', f'Font size of h2 is wrong for {text}'
+    assert font_weight == '700', f'Font weight of h2 is wrong for {text}'
+
+def assert_typography(browser):
+    sections = browser.find_many(xpath='//section')
+    hero_section = sections[0]
+    other_sections = sections[1:]
+    assert_hero_section(browser=browser, section=hero_section)
+    for other_section in other_sections:
+        assert_other_section(browser=browser, section=other_section)
+
+def assert_nothing_sticking_out(browser):    
+    # TODO: logic for this needs to be written
+    # Walk each section and ensure for every parent, children's boundaries are completely contained 
+    # within parents' boundaries 
+    pass
--- homepage/desktop/test_typography.py
+++ homepage/desktop/test_typography.py
@@ -2,6 +2,7 @@ import time
 from simplebrowser import SimpleBrowser
 import logging
 import pytest
+from common.asserts import assert_typography
 
 logger = logging.getLogger(__name__)
 
@@ -11,30 +12,5 @@ def browser(module_browser, base_url):
     module_browser.get(base_url)
     return module_browser
 
-def test_hero_section(browser):
-    sections = browser.find_many(xpath='//section')
-    section = sections[0]
-    h1s = section.find_elements_by_xpath('.//h1')
-    assert len(h1s) == 1, 'Hero section should have 1 h1'
-    h2s = section.find_elements_by_xpath('.//h2')
-    assert len(h2s) == 0, 'Hero section should have no h2s'
-
-def test_other_sections(browser):
-    sections = browser.find_many(xpath='//section')[1:]
-    for section in sections:
-        cl = section.get_attribute('class')
-        h2s = section.find_elements_by_xpath('.//h2')
-        assert len(h2s) == 1, f'Section with class {cl} has {len(h2s)} h2s'
-
-def test_h2_font(browser):
-    h2s = browser.find_many(xpath='//h2')
-    for h2 in h2s:
-        text = h2.text
-        font_size = h2.value_of_css_property('font-size')
-        font_weight = h2.value_of_css_property('font-weight')
-#        logger.info('checking h2 %s, font-size %s', text, font_size)
-        if 'Loved by leading finance' in h2.text:
-            assert font_size == '24px', 'Font size of logo section is wrong'
-        else:
-            assert font_size == '30px', f'Font size of h2 is wrong for {text}'
-        assert font_weight == '700', f'Font weight of h2 is wrong for {text}'
+def test_typography(browser):
+    assert_typography(browser=browser)
