f0c65dba5ecaed7a011527773093c678a4296974
debck19 <debasish.s@fyle.in>
2020-08-07 11:52:07 +0530

    add download feature form and collapsing tests

--- expense_page/test_expense_features.py
+++ expense_page/test_expense_features.py
@@ -1,6 +1,4 @@
-import os
 import time
-from simplebrowser import SimpleBrowser
 import logging
 import pytest
 from common.utils import resize_browser
@@ -12,15 +10,18 @@ def browser(module_browser, base_url, request):
     resize_browser(browser=module_browser, resolution=request.param)
     time.sleep(0.5)
     module_browser.get(base_url + "/expense-management")
-    time.sleep(3)
     return module_browser
 
 @pytest.mark.parametrize('browser', [('desktop_1')], indirect=True)
 def test_feature_scroll(browser):
-    result = False
     browser.click(xpath="//section[@class='feature-hero']//a[text()='Expense Reporting']")
     time.sleep(2)
     e = browser.find(xpath="//section[@id='expense-reporting']")
-    if abs(e.location['y'] - browser.current_scroll_position()) <= 30:
-        result = True
-    assert result, 'Not scrolling to the section'
+    assert abs(e.location['y'] - browser.current_scroll_position()) <= 30, 'Not scrolling to the Expense Reporting feature'
+
+@pytest.mark.parametrize('browser', [('desktop_1')], indirect=True)
+def test_collpasing_section(browser):
+    for i in range(1, 11):
+        section = browser.click(xpath=f"//a[@id='feature-{i}']")
+        class_list = section.get_attribute('class')
+        assert class_list.count('collapse-closed collapsed'), 'Collapsing of sections should work'
