b7d53069557686f2dde1d8b24136bc033845ff4e
debck19 <debasish.s@fyle.in>
2020-08-11 17:20:36 +0530

    fixed minor bugs in expense page

--- common/download_feature_list_form.py
+++ common/download_feature_list_form.py
@@ -1,19 +1,13 @@
-import time
+from time import sleep
 import logging
-import pytest
-from common.utils import resize_browser
 
 logger = logging.getLogger(__name__)
 
-@pytest.fixture(scope='function')
-def browser(module_browser, base_url, request):
-    resize_browser(browser=module_browser, resolution=request.param)
-    time.sleep(0.5)
-    module_browser.get(base_url + "/expense-management")
-    module_browser.find(xpath="//section[contains(@class, 'download-feature-list-section')]", scroll=True)
-    module_browser.click(xpath="//button[contains(@class, 'feature-list-download-btn')]")
-    time.sleep(1)
-    return module_browser
+def open_download_feature_form(browser):
+    sleep(2)
+    browser.find(xpath="//section[contains(@class, 'download-feature-list-section')]", scroll=True)
+    browser.click(xpath="//button[contains(@class, 'feature-list-download-btn')]")
+    sleep(1)
 
 def submit_download_feature_form(browser, email=None, firstname=None, lastname=None, phone=None, company_size=None, agree=None):
     if email:
@@ -31,21 +25,21 @@ def submit_download_feature_form(browser, email=None, firstname=None, lastname=N
         browser.click(xpath="//div[contains(@class, 'compare-feature-body')]//div[contains(@class, 'custom-checkbox')]")
     browser.click(xpath="//div[contains(@class, 'compare-feature-body')]//button[text()='Download']")
 
-@pytest.mark.parametrize('browser', [('desktop_1'), ('mobile_1')], indirect=True)
-def test_bad_email(browser):
+def assert_bad_email_download_feature_form(browser):
+    open_download_feature_form(browser)
     submit_download_feature_form(browser, email='foo')
     e = browser.find(xpath="//div[contains(@class, 'compare-feature-body')]//label[@for='demo-email-feature-download'][@class='error']")
     assert e and e.is_displayed(), 'No error displayed for invalid email'
 
-@pytest.mark.parametrize('browser', [('desktop_1'), ('mobile_1')], indirect=True)
-def test_missing_firstname(browser):
-    submit_download_feature_form(browser, email='megatron@fyle.in')
+def assert_missing_firstname_download_feature_form(browser):
+    open_download_feature_form(browser)
+    submit_download_feature_form(browser, email='test@fyle.in')
     e = browser.find(xpath="//div[contains(@class, 'compare-feature-body')]//label[@for='demo-first-name-feature-download'][@class='error demo-first-name-error']")
     assert e and e.is_displayed(), 'No error displayed for missing firstname'
 
-@pytest.mark.parametrize('browser', [('desktop_1'), ('mobile_1')], indirect=True)
-def test_success(browser):
-    submit_download_feature_form(browser, email='megatron@fyle.in', firstname='Megatron', lastname='Transformer', phone='123456789', company_size='Under 5', agree=True)
-    time.sleep(2)
+def assert_success_download_feature_form(browser):
+    open_download_feature_form(browser)
+    submit_download_feature_form(browser, email='test@fyle.in', firstname='Test', lastname='test', phone='123456789', company_size='Under 5', agree=True)
+    sleep(2)
     e = browser.find(xpath="//h3[contains(text(), 'Thank')]")
     assert e and e.is_displayed(), 'Not displaying thank you message'
